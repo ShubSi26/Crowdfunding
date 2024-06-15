f34b586098d823c7cddb07ac9ae87b2fd29e85a6
shubhamgit <git@gmail.com>
2024-06-15 23:35:19 +0530

    update

--- croudf/src/app/app.component.ts
+++ croudf/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'croudf';
+  title = 'Croudfunding';
   public static rooturl = "http://localhost:3000";
 }
--- croudf/src/app/bodycomp/bodycomp.component.ts
+++ croudf/src/app/bodycomp/bodycomp.component.ts
@@ -8,56 +8,60 @@ import $ from 'jquery';
 })
 export class BodycompComponent{
   constructor() {}
-
+  ngOnInit(): void {
+    $(document).ready(function(){
+      $(document).ready(function(){
+        $('#a').parent().mouseenter(function() {
+          let currentNumber = 0;
+          const targetNumber = 4928513;
+          let increment = 1;
+          const intervalTime = 2;
+      
+          const interval = setInterval(function() {
+            currentNumber += increment;
+            if (currentNumber >= targetNumber) {
+              currentNumber = targetNumber;
+              clearInterval(interval);
+            }
+            increment *= 1.1;
+            $('#a').text(Math.floor(currentNumber).toLocaleString('en-US') + "$+");
+          }, intervalTime);
+        });
+        $('#b').parent().mouseenter(function() {
+          let currentNumber = 0;
+          const targetNumber = 1000000;
+          let increment = 1;
+          const intervalTime = 2;
+      
+          const interval = setInterval(function() {
+            currentNumber += increment;
+            if (currentNumber >= targetNumber) {
+              currentNumber = targetNumber;
+              clearInterval(interval);
+            }
+            increment*=1.1;
+            $('#b').text(Math.floor(currentNumber).toLocaleString('en-US') + "+");
+          }, intervalTime);
+        });
+        $('#c').parent().mouseenter(function() {
+          let currentNumber = 0;
+          const targetNumber = 50000;
+          let increment = 1;
+          const intervalTime = 2;
+      
+          const interval = setInterval(function() {
+            currentNumber += increment;
+            if (currentNumber >= targetNumber) {
+              currentNumber = targetNumber;
+              clearInterval(interval);
+            }
+            increment*=1.1;
+            $('#c').text(Math.floor(currentNumber).toLocaleString('en-US') + "+");
+          }, intervalTime);
+        });
+      });
+      
+    });
+  }
 }
-$(document).ready(function(){
-  $('#a').parent().mouseenter(function() {
-    let currentNumber = 0;
-    const targetNumber = 4928513;
-    let increment = 1;
-    const intervalTime = 2;
-
-    const interval = setInterval(function() {
-      currentNumber += increment;
-      if (currentNumber >= targetNumber) {
-        currentNumber = targetNumber;
-        clearInterval(interval);
-      }
-      increment *= 1.1;
-      $('#a').text(Math.floor(currentNumber).toLocaleString('en-US') + "$+");
-    }, intervalTime);
-  });
-  $('#b').parent().mouseenter(function() {
-    let currentNumber = 0;
-    const targetNumber = 1000000;
-    let increment = 1;
-    const intervalTime = 2;
-
-    const interval = setInterval(function() {
-      currentNumber += increment;
-      if (currentNumber >= targetNumber) {
-        currentNumber = targetNumber;
-        clearInterval(interval);
-      }
-      increment*=1.1;
-      $('#b').text(Math.floor(currentNumber).toLocaleString('en-US') + "+");
-    }, intervalTime);
-  });
-  $('#c').parent().mouseenter(function() {
-    let currentNumber = 0;
-    const targetNumber = 50000;
-    let increment = 1;
-    const intervalTime = 2;
-
-    const interval = setInterval(function() {
-      currentNumber += increment;
-      if (currentNumber >= targetNumber) {
-        currentNumber = targetNumber;
-        clearInterval(interval);
-      }
-      increment*=1.1;
-      $('#c').text(Math.floor(currentNumber).toLocaleString('en-US') + "+");
-    }, intervalTime);
-  });
-});
 
